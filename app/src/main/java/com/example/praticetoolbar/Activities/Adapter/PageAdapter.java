754d80bcbc5df977ba6ddc75a8c676570a15c5dc
AndresTapiero72 <andres.tapiero@grability.com>
2019-10-12 18:19:15 -0500

    Add PageAdapter

---
+++ app/src/main/java/com/example/praticetoolbar/Activities/Adapter/PageAdapter.java
@@ -0,0 +1,35 @@
+package com.example.praticetoolbar.Activities.Adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import com.example.praticetoolbar.Fragments.FormFragment;
+import com.example.praticetoolbar.Fragments.ListFragment;
+
+public class PageAdapter extends FragmentStatePagerAdapter {
+
+    private int numberOfTabs;
+
+    public PageAdapter(FragmentManager fm, int numberofTabs) {
+        super(fm);
+        this.numberOfTabs = numberofTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+
+            case 0:
+                return new FormFragment();
+            case 1:
+                return new ListFragment();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return numberOfTabs;
+    }
+}
