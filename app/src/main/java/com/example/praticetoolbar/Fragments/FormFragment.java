845ae379f6c0b73412e3113ab1a4389b71a91fb1
AndresTapiero72 <andres.tapiero@grability.com>
2019-10-12 20:13:28 -0500

    Add logic Spinner List

--- app/src/main/java/com/example/praticetoolbar/Fragments/FormFragment.java
+++ app/src/main/java/com/example/praticetoolbar/Fragments/FormFragment.java
@@ -5,19 +5,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.praticetoolbar.R;
 
 
 public class FormFragment extends Fragment {
+    private Spinner opciones;
+
     public FormFragment() {
         // Required empty public constructor
     }
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_form, container, false);
+         View view = inflater.inflate(R.layout.fragment_form, container, false);
+
+         //Spinner Logic
+         opciones =  view.findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.Paises,android.R.layout.simple_spinner_dropdown_item
+        );
+        opciones.setAdapter(adapter);
+
+
+        return view;
     }
 
 }
